fb2b5c7efd9b082ce3ca1de73531d5d398f6c3a1
VangaPriyanka <priyankavanga44@gmail.com>
2026-05-21 15:49:58 +0530

    Day 3: Added backend API routes for all panels

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
-
+const userRoutes = require("./routes/userRoutes");
+const doctorRoutes = require("./routes/doctorRoutes");
+const patientRoutes = require("./routes/patientRoutes");
+const appointmentRoutes = require("./routes/appointmentRoutes");
 const app = express();
 
+app.use("/api/users", userRoutes);
+app.use("/api/doctors", doctorRoutes);
+app.use("/api/patients", patientRoutes);
+app.use("/api/appointments", appointmentRoutes);
 app.get("/", (req, res) => {
     res.send("MedXpert Backend Running");
 });
